e92abefae63cae8b5b453a7948cc941af074386a
Achraf Bizyane <abizyane@e1r2p2.1337.ma>
2024-06-07 15:24:15 +0100

    portainer admin configuration at startup [FIXED]

--- srcs/requirements/bonus/portainer/tools/setup.sh
+++ srcs/requirements/bonus/portainer/tools/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-HASHED_PASSWORD=$(htpasswd -nbBC 10 "" $PORTAINER_PASS | tr -d ':\n')
+exec /portainer/portainer &
+PORTAINER_PID=$!
 
-exec /portainer/portainer --admin-password=$HASHED_PASSWORD 
+sleep 10
+
+curl -X POST "http://localhost:9000/api/users/admin/init" -H "Content-Type: application/json" --data "{\"Username\":\"$PORTAINER_NAME\",\"Password\":\"$PORTAINER_PASS\"}"
+
+wait $PORTAINER_PID
