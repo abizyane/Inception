e399524bcc93bb41b7aa58c3d6834185d166b108
Achraf Bizyane <abizyane@e1r2p2.1337.ma>
2024-06-07 14:16:04 +0100

    portainer [FIXED]

--- srcs/requirements/bonus/portainer/tools/setup.sh
+++ srcs/requirements/bonus/portainer/tools/setup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-cd /opt/portainer
+HASHED_PASSWORD=$(htpasswd -nbBC 10 "" $PORTAINER_PASS | tr -d ':\n')
 
-sed -i 's/localhost/0.0.0.0/g' app/docker/views/proxy/initEndpointProxy.js
-
-touch .env
-
-yarn install  --legacy-peer-deps
-
-yarn build
-
-exec yarn webpack-dev-server --host=0.0.0.0 --port=9000
-
-
-# &
-# sleep 10
-
-# # Use the Portainer API to automate sign-in process
-# curl -H "Content-Type: application/json" \
-#      -d '{"Username": "admin", "Password": "password"}' \
-#      http://localhost:9000/api/auth
+exec /portainer/portainer --admin-password=$HASHED_PASSWORD 
